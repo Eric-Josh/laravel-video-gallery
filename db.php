a532f6602dd744c8d5326096f035b6b738369d82
Joshua Ezeh <techjosmar@gmail.com>
2020-11-16 21:16:12 +0100

    Initial Commit

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "192.168.30.8";
-  $username = "geepcmdc";
-  $password = "3ndl355";
-  $dbname = "ebis_new";
+$servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "db_table";
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
